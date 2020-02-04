e9484e8b62b34c0f7856c54ed34c556f8573a235
KaiserinDerWelt <42034666+KaiserinDerWelt@users.noreply.github.com>
2020-02-03 20:20:08 -0600

    Add files via upload

---
+++ Multiples_Of_8_Till_190/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Multiples_Of_8_Till_190
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print Multiples of 8 till 190");
+            int StartValue, Limit;
+            for(StartValue = 1; StartValue <= 23; StartValue += 1)
+                {
+                Limit = StartValue * 8;
+                Console.WriteLine(Limit);
+            }
+        }
+    }
+}
