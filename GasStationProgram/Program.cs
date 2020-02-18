03c1bcb8e9c268e080e17ec26a667b7a6586bb9e
KaiserinDerWelt <42034666+KaiserinDerWelt@users.noreply.github.com>
2020-02-17 19:00:21 -0600

    Add files via upload

---
+++ GasStationProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DPRN1_U2_A3_MALA
+{
+   public class TanqueDeGasolina //Creacion de clase.
+    {//Declaracion de atributos de clase
+        public double CapacidadMaxima, CantidadActual;
+
+        //Constructor Predeterminado
+        public TanqueDeGasolina() {
+           
+        }
+
+}
+//La definicion de clases se hacer por arriba de la clase programa.
+class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
+
