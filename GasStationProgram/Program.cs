0424ae8150fee3a4643825f3b44574c237603983
KaiserinDerWelt <42034666+KaiserinDerWelt@users.noreply.github.com>
2020-02-20 19:28:21 -0600

    Update Program.cs

--- GasStationProgram/Program.cs
+++ GasStationProgram/Program.cs
@@ -1,17 +1,28 @@
-﻿using System;
+using System;
 
 namespace DPRN1_U2_A3_MALA
 {
    public class TanqueDeGasolina //Creacion de clase.
     {//Declaracion de atributos de clase
-        public double CapacidadMaxima, CantidadActual;
+        public double CapacidadMaxima, CantidadActual,Reserva;
+      
 
         //Constructor Predeterminado
         public TanqueDeGasolina() {
-           
+            CapacidadMaxima = 42; //Declaracion de la cantidad Maxima en 42
+            CantidadActual = 2;  //Declaracion de la cantidad 2
         }
 
-}
+        public void CapacidadTanque (){
+            CantidadActual = CapacidadMaxima;
+        }
+        public void AjusteMax() {
+            //Sentencia Condicional
+            if (2 > CapacidadMaxima){
+                CantidadActual = CapacidadMaxima;
+            }
+        //To-do el codigo demas perteneciente a la clase TanqueDeGasolina
+    }
 //La definicion de clases se hacer por arriba de la clase programa.
 class Program
     {
@@ -20,5 +31,6 @@ class Program
            
         }
     }
+}
 }
 
